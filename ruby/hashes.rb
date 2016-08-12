a205a015bb65a8774ebab4a2d5817339fe2130cb
Alex Perez <ap2539@gmail.com>
2016-08-11 19:25:04 -0700

    Create hash with user data, print hash and change variable names

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -9,15 +9,15 @@ name = get_input("Name: ")
 address = get_input("Address: ")
 email = get_input("Email: ")
 age = get_input("Age: ").to_i
-num_child = get_input("Number of Children: ").to_i
-likes_chevrons = get_input("Likes Chevrons? ")
+number_of_children = get_input("Number_of_children: ").to_i
+likes_chevrons = get_input("likes_chevrons: ")
 	#translate user input to boolen
-	if likes_chevrons.upcase == "NO"
+	if likes_chevrons.upcase == "NO" || likes_chevrons == "FALSE"
 		likes_chevrons = false
 	else
 		likes_chevrons = true
 	end
-decor = get_input("Decor Theme: ")
+decor_theme = get_input("Decor Theme: ")
 
 #Print individual variable results for testing
 #p name
@@ -27,5 +27,21 @@ decor = get_input("Decor Theme: ")
 #p num_child
 #p likes_chevrons
 #p decor
+
+# create hash
+client = {
+	name: name,
+	address: address,
+	email: email,
+	age: age,
+	number_of_children: number_of_children,
+	likes_chevrons: likes_chevrons,
+	decor_theme: decor_theme
+}
+
+#print hash for user
+p client
+
+
 
 
