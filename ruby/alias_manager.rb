4185a89d4194bd5de00a610b35da850235cc32f5
Alex Perez <ap2539@gmail.com>
2016-08-14 08:36:12 -0700

    Add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,21 @@
+#Split take the current name and split it to an array with first name and an array with last name
+#pass the arrays to the get chars method to split it into individual letters
+	#use .chars to resave the array to an array of teh string letters
+#pass the array of letters to the chnage vowels method
+	#takes an array of letters 
+	#create a loop to cycle through each letter
+	#check if each letter is a vowel by comparing against a static array that contains all vowels
+	#if the letter is a vowel
+		#add 1 to the current index postition of the vowel array
+		#save the current position of the vowel to the current position of the letter array
+	#return the updated array
+#pass the array of letters to the change consonant method
+		#takes an array of letters 
+	#create a loop to cycle through each letter
+	#check if each letter is a vowel by comparing against a static array that contains all vowels
+	#if the letter is a vowel
+		#add 1 to the current index postition of the vowel array
+		#save the current position of the vowel to the current position of the letter array
+	#return the updated array
+#print the modified name
+
