3b5664fc6d2dd9e82f5d78dd20bfe35f4c3fcf4e
Alex Perez <ap2539@gmail.com>
2016-08-17 09:04:11 -0700

    create new method speak and call it

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,8 +5,16 @@ class Puppy
     toy
   end
 
+  def speak(speak_times)
+  	speak_times.times do |speak|
+		puts "Woof!"  		
+  	end
+  end
+
 end
 
 pixel = Puppy.new
 pixel.fetch("ball")
+
+pixel.speak(2)
 
