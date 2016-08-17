faf1b14a77796afdf747c9b3e90e322a412eeffa
Alex Perez <ap2539@gmail.com>
2016-08-17 09:12:02 -0700

    create new method to calculate dog years from human years and clean up comments

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -15,12 +15,25 @@ class Puppy
   	puts "*rolls over*"
   end
 
+  def dog_years(human_years)
+  	dog_years = human_years * 7  	
+  end
+
 end
 
+#create new puppy instance
 pixel = Puppy.new
+
+#play with puppy instance
 pixel.fetch("ball")
 
+#ask puppy instance to speak
 pixel.speak(2)
 
+#ask puppy instance to roll over
 pixel.roll_over
 
+#ask how old puppy instance is if you only know human years
+human_years = 2
+dog_years = pixel.dog_years(human_years)
+puts dog_years
