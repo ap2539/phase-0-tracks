260490ac5ae6f103b08cdea3e7be5e00a1faa1ae
Alex Perez <ap2539@gmail.com>
2016-08-20 16:59:32 -0700

    Add user interface to allow user to create objects

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -74,18 +74,89 @@ class Pokemon
 	end
 end
 
-name = "Pikachu"
-moves = ["Electroball", "Quick Attack", "Iron Tail", "Thunder Bolt"]
-level = 20
+#name = "Pikachu"
+#moves = ["Electroball", "Quick Attack", "Iron Tail", "Thunder Bolt"]
+#level = 20
 
 
-pikachu = Pokemon.new(name,moves,level)
+#pikachu = Pokemon.new(name,moves,level)
 
-pikachu.display_stats
-pikachu.attack
-pikachu.level_up
-pikachu.level_up
-pikachu.display_stats
-pikachu.change_move("Electroball","Thunder")
-pikachu.display_stats
+#pikachu.display_stats
+#pikachu.attack
+#pikachu.level_up
+#pikachu.level_up
+#pikachu.display_stats
+#pikachu.change_move("Electroball","Thunder")
+#pikachu.display_stats
+
+pokemon_team = []
+#sample moves used later to randomly assign a move to a pokemon
+sample_moves = ["Quick Attack", "Tackle", "Scratch", "Growl", 
+				"Leer", "Headbut", "Assist", "Boomburst", 
+				"Double Team", "Flash", "Focus Energy", 
+				"Double Slap", "Mega Punch", "Mean Look"]
+
+
+#A pokemon trainer can only have up to 6 pokemon at any given time!!!
+until pokemon_team.length >= 6
+	#reset the input variables
+	user_moves = []
+	moves_input = ""
+	user_pokemon_name = ""
+	user_level = 1
+
+	puts "Please enter Pokemon name or 'done' to finish"
+	user_pokemon_name = gets.chomp
+	
+	#you can also break if the user types done
+	break if user_pokemon_name.downcase == 'done'
+
+	#a pokemon must have 4 moves
+	until user_moves.length >= 4 || moves_input.downcase == 'random'
+		puts "Please enter a pokemon move for #{user_pokemon_name}: #{4 - user_moves.length} more move(s) needed (or type 'random')"
+		moves_input = gets.chomp
+
+		#give the user the option randomly assign moves
+		if moves_input.downcase == "random" && user_moves.length == 0
+			4.times do 
+				user_moves << sample_moves.sample
+			end
+		else
+				user_moves << moves_input
+		end
+	end
+
+	#collect the level of the pokemon
+	puts "Please enter #{user_pokemon_name}'s level"
+	user_level = gets.chomp.to_i
+
+	#create pokemon and add to the pokemon team array
+	pokemon_team << Pokemon.new(user_pokemon_name, user_moves, user_level)
+end
+	
+	#print the pokemon stats after loop is done
+	pokemon_team.each do |pokemon|
+		pokemon.display_stats
+		puts "\n"	
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
