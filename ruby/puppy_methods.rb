a79228b568609e98325e20c5dd466cb8319e5d3e
Alex Perez <ap2539@gmail.com>
2016-08-20 09:15:17 -0700

    create new class and add 2 new class methods to it

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,30 +1,37 @@
 class Puppy
+	def initialize
+		puts "Initializing new puppy instance ..."
+	end 
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def method_name
+	
+	end
 
-  def speak(speak_times)
-  	speak_times.times do |speak|
-		puts "Woof!"  		
+  	def fetch(toy)
+    	puts "I brought back the #{toy}!"
+    	toy
   	end
-  end
 
-  def roll_over
-  	puts "*rolls over*"
-  end
+  	def speak(speak_times)
+  		speak_times.times do |speak|
+		puts "Woof!"  		
+		end
+  	end
 
-  def dog_years(human_years)
-  	dog_years = human_years * 7  	
-  end
+  	def roll_over
+  		puts "*rolls over*"
+  	end
 
-  def cutness_overload
-  	puts "  __      _"
-  	puts "o'')}____//"
-  	puts " `_/      )"
-  	puts " (_(_/-(_/"
-  end
+  	def dog_years(human_years)
+  		dog_years = human_years * 7  	
+  	end
+
+  	def cutness_overload
+  		puts "  __      _"
+  		puts "o'')}____//"
+  		puts " `_/      )"
+  		puts " (_(_/-(_/"
+  	end
 end
 
 #create new puppy instance
@@ -45,4 +52,25 @@ dog_years = pixel.dog_years(human_years)
 puts dog_years
 
 pixel.cutness_overload
+
+
+
+class Pokemon
+	def initialize(name)
+		@pokemon_name = name
+		puts "#{@pokemon_name} I choose you..."
+	end 
+
+	def attack(move)
+		puts "#{@pokemon_name} used #{move} ... Its Super Effective!"
+	end
+
+	def dodge
+		puts "#{@pokemon_name} dodged the attack!"
+	end
+end
+
+pikachu = Pokemon.new("Pikachu")
+pikachu.attack("Thunder")
+pikachu.dodge
 
