03fa3c536ddad26a7c1685c636d4b319db808a63
Alex Perez <ap2539@gmail.com>
2016-08-27 18:16:19 -0700

    Create loop to combine color and horse in a single hash object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,5 @@
 var colors = ["teal","purple","black","blue"];
-var horses = ["Jared","Ashely","Izzy","Grant"];
+var horses = ["Jared","Ashley","Izzy","Grant"];
 
 console.log(colors);
 console.log(horses);
@@ -9,3 +9,9 @@ horses.push("Tommy");
 
 console.log(colors);
 console.log(horses);
+
+var horse_list = {}
+for (var i = 0; i < horses.length; i++) {
+	horse_list[horses[i]] = colors[i];
+	console.log(horse_list);
+}
