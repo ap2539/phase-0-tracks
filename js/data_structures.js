eadaaca19a695250370ef0ba79694ab0d2700dda
Alex Perez <ap2539@gmail.com>
2016-08-27 23:35:16 -0700

    created car class

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,22 @@ for (var i = 0; i < horses.length; i++) {
 	horse_list[horses[i]] = colors[i];
 	console.log(horse_list);
 }
+
+function Car(model, year, maintenance) {
+	this.model = model;
+	this.year = year;
+	this.maintenance = maintenance;
+
+	this.fix_car = function() {
+		if (maintenance) {
+			console.log("Fixed the car");
+			this.maintenance = false;
+		} else {
+			console.log("Nothing to fix");
+		}
+	}
+}
+
+var car = new Car("Accord", 2016, true);
+car.fix_car();
+console.log(car);
