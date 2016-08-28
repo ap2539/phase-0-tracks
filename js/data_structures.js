8510eb7678edc591788edaa6f6f5b06cdd61573b
Alex Perez <ap2539@gmail.com>
2016-08-27 18:05:54 -0700

    Create two new arrays and add new items to the arrays

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+var colors = ["teal","purple","black","blue"];
+var horses = ["Jared","Ashely","Izzy","Grant"];
+
+console.log(colors);
+console.log(horses);
+
+colors.push("green");
+horses.push("Tommy");
+
+console.log(colors);
+console.log(horses);
