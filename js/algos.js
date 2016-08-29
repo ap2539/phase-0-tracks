a05edc8f1c72e2de62e23bb6559dd290a249e4e8
Alex Perez <ap2539@gmail.com>
2016-08-29 09:43:00 -0700

    Created fucntion tell if values match between two hash objects

--- js/algos.js
+++ js/algos.js
@@ -9,6 +9,22 @@
 //Output: return the string value of the longestIndex
 
 
+//method: does_match
+//input: takes 2 hash objects
+//steps: intialize 2 arrays one for each hash and default a boolean to false
+//steps; split the hashs up to an array of values
+//steps: loop through the first array
+	//steps: if the second array includes a value from teh first array 
+		//Steps: set match = true
+//output: boolean
+
+//method: random_word
+//input: integer -> array size
+//Steps: for each value in array size
+	//push and random word to new array
+//output: new array
+
+
 function get_longest(phraseArray) {
 	var longestIndex = 0;
 	for (var i = 0; i < phraseArray.length; i++) {
@@ -17,6 +33,28 @@ function get_longest(phraseArray) {
 		}
 	}
 	return phraseArray[longestIndex];
+}
+
+function does_match(first_hash,second_hash) {
+	var first_array_values = new Array();
+	var second_array_values = new Array();
+	var match = false;
+
+	//refactor this section to a seperate function
+	for (var key in first_hash) {
+    	first_array_values.push(first_hash[key]);
+	}
+	for (var key in second_hash) {
+    	second_array_values.push(second_hash[key]);
+	}
+
+	//loop through the first array and compare against the second array for matches
+	for (var i=0; i < first_array_values.length; i++) {
+		if (second_array_values.includes(first_array_values[i])) {
+			match = true;
+		}
+	}
+	return match;
 }
 
 var array_of_phrases = ["long phrase", "long phrase","long phrase","longest phrase","longer phrase"];
@@ -28,3 +66,12 @@ console.log(get_longest(array_of_phrases));
 array_of_phrases = ["1", "12", "123", "1234", "1234567", "123456"];
 console.log(get_longest(array_of_phrases));
 
+var hash_of_data = {name: "Steven", age: 54};
+var hash_of_data2 = {name: "Alex", age: 54};
+console.log(does_match(hash_of_data,hash_of_data2));
+
+hash_of_data = {name: "Steven", age: 20};
+hash_of_data2 = {name: "Alex", age: 54};
+console.log(does_match(hash_of_data,hash_of_data2));
+
+var int = 20;
